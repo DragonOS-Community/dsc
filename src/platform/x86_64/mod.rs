a58b266823742b2a97575651af037e5e0f6f5aa1
longjin <longjin@RinGoTek.cn>
2023-10-24 08:12:15 +0000

    调整系统调用的寄存器传参顺序

--- src/platform/x86_64/mod.rs
+++ src/platform/x86_64/mod.rs
@@ -19,7 +19,7 @@ pub unsafe fn syscall1(mut n: usize, a1: usize) -> usize {
     asm!(
         "int 0x80",
         inout("rax") n,
-        in("r8") a1,
+        in("rdi") a1,
         out("rcx") _,
         out("r11") _,
         options(nostack),
@@ -32,8 +32,8 @@ pub unsafe fn syscall2(mut n: usize, a1: usize, a2: usize) -> usize {
     asm!(
         "int 0x80",
         inout("rax") n,
-        in("r8") a1,
-        in("r9") a2,
+        in("rdi") a1,
+        in("rsi") a2,
         out("rcx") _,
         out("r11") _,
         options(nostack),
@@ -46,9 +46,9 @@ pub unsafe fn syscall3(mut n: usize, a1: usize, a2: usize, a3: usize) -> usize {
     asm!(
         "int 0x80",
         inout("rax") n,
-        in("r8") a1,
-        in("r9") a2,
-        in("r10") a3,
+        in("rdi") a1,
+        in("rsi") a2,
+        in("rdx") a3,
         out("rcx") _,
         out("r11") _,
         options(nostack),
@@ -57,92 +57,66 @@ pub unsafe fn syscall3(mut n: usize, a1: usize, a2: usize, a3: usize) -> usize {
 }
 
 #[inline(always)]
-pub unsafe fn syscall4(mut n: usize,
-                       a1: usize,
-                       a2: usize,
-                       a3: usize,
-                       a4: usize)
-                       -> usize {
+pub unsafe fn syscall4(mut n: usize, a1: usize, a2: usize, a3: usize, a4: usize) -> usize {
     asm!(
         "int 0x80",
         inout("rax") n,
-        in("r8") a1,
-        in("r9") a2,
-        in("r10") a3,
-        in("r11") a4,
+        in("rdi") a1,
+        in("rsi") a2,
+        in("rdx") a3,
+        in("r10") a4,
         out("rcx") _,
+        out("r11") _,
         options(nostack),
     );
     n
 }
 
 #[inline(always)]
-pub unsafe fn syscall5(mut n: usize,
-                       a1: usize,
-                       a2: usize,
-                       a3: usize,
-                       a4: usize,
-                       a5: usize)
-                       -> usize {
+pub unsafe fn syscall5(
+    mut n: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+) -> usize {
     asm!(
         "int 0x80",
         inout("rax") n,
-        in("r8") a1,
-        in("r9") a2,
-        in("r10") a3,
-        in("r11") a4,
-        in("r12") a5,
+        in("rdi") a1,
+        in("rsi") a2,
+        in("rdx") a3,
+        in("r10") a4,
+        in("r8") a5,
         out("rcx") _,
+        out("r11") _,
         options(nostack),
     );
     n
 }
 
 #[inline(always)]
-pub unsafe fn syscall6(mut n: usize,
-                       a1: usize,
-                       a2: usize,
-                       a3: usize,
-                       a4: usize,
-                       a5: usize,
-                       a6: usize)
-                       -> usize {
+pub unsafe fn syscall6(
+    mut n: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+    a6: usize,
+) -> usize {
     asm!(
         "int 0x80",
         inout("rax") n,
-        in("r8") a1,
-        in("r9") a2,
-        in("r10") a3,
-        in("r11") a4,
-        in("r12") a5,
-        in("r13") a6,
+        in("rdi") a1,
+        in("rsi") a2,
+        in("rdx") a3,
+        in("r10") a4,
+        in("r8") a5,
+        in("r9") a6,
         out("rcx") _,
-        options(nostack),
-    );
-    n
-}
-
-#[inline(always)]
-pub unsafe fn syscall7(mut n: usize,
-                       a1: usize,
-                       a2: usize,
-                       a3: usize,
-                       a4: usize,
-                       a5: usize,
-                       a6: usize,
-                       a7: usize)
-                       -> usize {
-    asm!(
-        "int 0x80",
-        inout("rax") n,
-        in("r8") a1,
-        in("r9") a2,
-        in("r10") a3,
-        in("r11") a4,
-        in("r12") a5,
-        in("r13") a6,
-        in("r14") a7,
-        out("rcx") _,
+        out("r11") _,
         options(nostack),
     );
     n
